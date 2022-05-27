c8af2c5f6563aae2fe448c23a4b8f6b85c610c9e
hoangkhai <hoangkhai6893@gmail.com>
2022-05-27 13:13:52 +0900

    add etl.ipynb

---
+++ etl_runOnLocal.py
@@ -0,0 +1,18 @@
+import configparser
+from datetime import datetime
+import os
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf, col
+from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
+from pyspark.sql import functions as F
+from pyspark.sql import types as T
+import pandas as pd
+from schema import *
+
+config = configparser.ConfigParser()
+#Normally this file should be in ~/.aws/credentials
+config.read_file(open('dl.cfg'))
+
+
+os.environ["AWS_ACCESS_KEY_ID"]= config['AWS']['AWS_ACCESS_KEY_ID']
+os.environ["AWS_SECRET_ACCESS_KEY"]= config['AWS']['AWS_SECRET_ACCESS_KEY']
